966ab4e59fc94454e120d850b0e712a859044650
1651680599 <1651680599@qq.com>
2022-05-14 19:05:16 +0800

    Revert "add Qwe1"
    This reverts commit e722fd9c

--- src/main/java/com/hsy/entity/Qwe.java
+++
@@ -1,9 +0,0 @@
-package com.hsy.entity;
-
-public class Qwe {
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-        //return super.clone();
-    }
-}
