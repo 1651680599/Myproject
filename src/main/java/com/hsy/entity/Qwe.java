e722fd9c7d03ec4a58a62627723bfab7b5237dfe
1651680599 <1651680599@qq.com>
2022-05-14 19:02:54 +0800

    add Qwe1

---
+++ src/main/java/com/hsy/entity/Qwe.java
@@ -0,0 +1,9 @@
+package com.hsy.entity;
+
+public class Qwe {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+        //return super.clone();
+    }
+}
