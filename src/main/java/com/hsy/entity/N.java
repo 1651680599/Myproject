5049db41cbaa67b387bc6ced729afe5ec8c1c570
1651680599 <1651680599@qq.com>
2022-05-18 21:51:48 +0800

    test undo add M N

--- src/main/java/com/hsy/entity/N.java
+++ src/main/java/com/hsy/entity/N.java
@@ -1,4 +1,15 @@
 package com.hsy.entity;
 
 public class N {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    protected String narch;
+
+    protected String narch1;
+
+    protected String narch2;
 }
